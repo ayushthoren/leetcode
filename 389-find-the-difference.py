bd0badee25ef36a8406bac5239887cc1597d59fd
Ayush Thoren <ayushthoren@gmail.com>
2022-02-03 03:02:25 +0000

    Runtime: 43 ms, Memory 13.8 MB

--- 389-find-the-difference.py
+++ 389-find-the-difference.py
@@ -1,6 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        #For each letter i nthe strign with the added character
+        #For each letter in the strign with the added character
         for i in t:
             #If the current letter was added to t, return it
             if t.count(i)!=s.count(i): return i
