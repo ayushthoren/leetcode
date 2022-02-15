e4ec96e40941e4cc151a87d2a88f3edc47753969
Ayush Thoren <ayushthoren@gmail.com>
2022-02-15 05:00:55 +0000

    Runtime: 737 ms, Memory 16.8 MB

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+      dist=0
+      dists=[]
+      for i in nums:
+        if i==0: dist+=1
+        if i==1:
+          dists.append(dist)
+          dist=0
+      print(dists)
+      for j in dists[1:]:
+        if j<k: return False
+      return True
