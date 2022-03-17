98db5d93e2a3a254be57fcc0fe9823a421548061
Ayush Thoren <ayushthoren@gmail.com>
2022-03-17 02:16:00 +0000

    Runtime: 51 ms, Memory 13.8 MB

---
+++ 13-roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+      map = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+      }
+      total = 0
+      for i in range(len(s)-1):
+        if map[s[i]] >= map[s[i+1]]: total+=map[s[i]]
+        else: total-=map[s[i]]
+      return total+map[s[-1]]
