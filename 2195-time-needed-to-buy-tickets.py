316fff522cead0adbcecef9a8073d60a5f428319
Ayush Thoren <ayushthoren@gmail.com>
2022-05-01 03:02:19 +0000

    Runtime: 73 ms, Memory 13.8 MB

---
+++ 2195-time-needed-to-buy-tickets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+      time=0
+      while tickets[k]>0:
+        for i in range(len(tickets)):
+          if tickets[k]==0: break
+          if tickets[i]>0:
+            tickets[i]-=1
+            time+=1
+      return time
