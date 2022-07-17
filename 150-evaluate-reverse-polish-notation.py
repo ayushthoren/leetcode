9f092c753f147c58d197d0569a757390519de304
Ayush Thoren <ayushthoren@gmail.com>
2022-07-17 19:26:20 +0000

    Runtime: 306 ms, Memory 14.5 MB

---
+++ 150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+      operators=["+","-","*","/"]
+      stack=[]
+      for i in tokens:
+        if i in operators:
+          if i=="/": new=str(int(eval(stack[-2]+i+stack[-1])))
+          else: new=str(eval(stack[-2]+i+stack[-1]))
+          stack=stack[:-2]
+          stack.append(new)
+        else: stack.append(i)
+      return stack[0]
