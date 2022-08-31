81bb12273fbf2cc6eb8f80dc39076d98ba3d7bd8
Ayush Thoren <ayushthoren@gmail.com>
2022-08-31 03:14:45 +0000

    Runtime: 98 ms, Memory 13.5 MB

---
+++ 2048-build-array-from-permutation.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [nums[nums[i]] for i in range(len(nums))]
