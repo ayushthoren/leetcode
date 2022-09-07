5c4e5ff54bc335ccb14c12369d2068cf3d385c1b
Ayush Thoren <ayushthoren@gmail.com>
2022-09-07 03:22:21 +0000

    Runtime: 81 ms, Memory 13.4 MB

---
+++ 837-most-common-word.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+      paragraph=paragraph.lower()
+      punctuation=[" ",".",",","!","?",";",":","\'"]
+      para=[""]
+      for i in paragraph:
+        if i in punctuation: 
+          if para[-1]!="": para.append("")
+        else: para[-1]=para[-1]+i
+      print(para)
+      cts={}
+      for j in para:
+        if j not in banned and j!="":
+          if j not in cts: cts[j]=0
+          cts[j]+=1
+      return max(cts,key=cts.get)
