c13a4952cefe35289023585c74c98dc89b124664
Ayush Thoren <ayushthoren@gmail.com>
2022-10-12 02:04:31 +0000

    Runtime: 2472 ms, Memory 14.3 MB

---
+++ 2486-most-frequent-even-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+      nums=[e for e in sorted(nums) if e%2==0]
+      maxCt=0
+      mins=[]
+      for i in nums:
+        iCt=nums.count(i)
+        if iCt>maxCt:
+          maxCt=nums.count(i)
+          mins=[i]
+        if iCt==maxCt:
+          mins.append(i)
+      if len(mins)==0: return -1
+      return min(mins)
