b62b82695f4b7bc7e0ff84c7bf6ace8d9d8244c1
Ayush Thoren <ayushthoren@gmail.com>
2023-06-06 01:38:29 +0000

    Runtime: 56 ms, Memory 16.3 MB

--- 1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude.py
@@ -1,5 +1,7 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        alts=[0]
-        for i in gain: alts.append(alts[-1]+i)
-        return max(alts)
+        alts,ma=[0],0
+        for i in gain:
+            alts.append(alts[-1]+i)
+            if alts[-1]>ma: ma=alts[-1]
+        return ma
