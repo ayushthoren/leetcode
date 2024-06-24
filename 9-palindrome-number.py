96f8499a41ba72ad961c2ca0ea99195778eb125b
Ayush Thoren <ayushthoren@gmail.com>
2024-06-24 15:29:59 +0000

    Runtime: 71 ms, Memory 16.5 MB

--- 9-palindrome-number.py
+++ 9-palindrome-number.py
@@ -1,3 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x)[::-1]==str(x)
+        if x<0: return False
+        rev,tmp=0,x
+        while tmp!=0: rev=(rev*10)+(tmp%10); tmp//=10
+        return rev==x
