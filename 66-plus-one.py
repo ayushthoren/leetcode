c02bd1290db063b5b4902fcf71a49b54aca5c569
Ayush Thoren <ayushthoren@gmail.com>
2024-07-01 15:25:30 +0000

    Runtime: 40 ms, Memory 16.5 MB

--- 66-plus-one.py
+++ 66-plus-one.py
@@ -1,3 +1,3 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        return [j for j in str(int("".join([str(i) for i in digits]))+1)]
+        return [int(j) for j in str(int("".join([str(i) for i in digits]))+1)]
