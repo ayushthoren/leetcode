00128a366ca56410c91150609fad8cda26cfe18d
Ayush Thoren <ayushthoren@gmail.com>
2024-07-15 02:23:24 +0000

    Runtime: 108 ms, Memory 29.5 MB

--- 208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree.py
@@ -1,16 +1,28 @@
 class Trie:
 
-    def __init__(self):
-        self.words=[]
-
-    def insert(self, word: str) -> None:
-        self.words.append(word)
-
-    def search(self, word: str) -> bool:
-        return word in self.words
-
-    def startsWith(self, prefix: str) -> bool:
-        return any(i.startswith(prefix) for i in self.words)
+  def __init__(self):
+    self.words={}
+  
+  def insert(self, word: str) -> None:
+    cur=self.words
+    for i in word:
+      if i not in cur: cur[i]={}
+      cur=cur[i]
+    cur["end"]=None
+  
+  def search(self, word: str) -> bool:
+    cur=self.words
+    for i in word:
+      if i not in cur: return False
+      cur=cur[i]
+    return "end" in cur
+  
+  def startsWith(self, prefix: str) -> bool:
+    cur=self.words
+    for i in prefix:
+      if i not in cur: return False
+      cur=cur[i]
+    return True
 
 
 # Your Trie object will be instantiated and called as such:
