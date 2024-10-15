0acc74b07b37e5bd5465d29d88d21aa6efe845aa
Ayush Thoren <ayushthoren@gmail.com>
2024-10-15 02:27:30 +0000

    Runtime: 294 ms, Memory 19.1 MB

--- 455-assign-cookies.py
+++ 455-assign-cookies.py
@@ -1,8 +1,8 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g,s=sorted(g),sorted(s)
-        ct=0
-        for i in g:
-            for j in range(len(s)):
-                if s[j]>=i: s[j]=0; ct+=1; break
+        p1,p2,ct=0,0,0
+        while p1<len(g) and p2<len(s):
+            if g[p1]<=s[p2]: ct+=1; p1+=1
+            p2+=1
         return ct
