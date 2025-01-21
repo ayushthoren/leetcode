7ff3f745f8ab2e94177e56461487b31c9e000cb1
Ayush Thoren <ayushthoren@gmail.com>
2025-01-21 21:55:48 +0000

    Runtime: 7 ms, Memory 20 MB

---
+++ 57-insert-interval.py
@@ -0,0 +1,15 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: x[0])
+        good,ln=False,len(intervals)
+        while not good:
+            good=True
+            for i in range(ln-1):
+                if intervals[i][1]>=intervals[i+1][0]:
+                    if intervals[i+1][1]>intervals[i][1]:
+                        intervals[i][1]=intervals[i+1][1]
+                    intervals.pop(i+1); ln-=1
+                    good=False
+                    break
+        return intervals
