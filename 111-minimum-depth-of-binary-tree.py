fef073ac45fd0b5995a5ed947b4bdfb2d93802f8
Ayush Thoren <ayushthoren@gmail.com>
2025-02-26 04:33:36 +0000

    Runtime: 4 ms, Memory 50.5 MB

---
+++ 111-minimum-depth-of-binary-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        d,q=0,[root]
+        while True:
+            d+=1
+            new=[]
+            for i in q:
+                if i.left: new.append(i.left)
+                if i.right: new.append(i.right)
+                if not i.left and not i.right: return d
+            q=new
