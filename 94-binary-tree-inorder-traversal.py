23d8f695195b6d07954697ec03388ec4ccbaf7b7
Ayush Thoren <ayushthoren@gmail.com>
2025-03-20 17:30:15 +0000

    Runtime: 0 ms, Memory 17.9 MB

--- 94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal.py
@@ -5,13 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def helperInorder(self, root: Optional[TreeNode], arr) -> List[int]:
-      if root==None: return
-      self.helperInorder(root.left, arr)
-      arr.append(root.val)
-      self.helperInorder(root.right, arr)
-      return arr
-
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-      if root == None: return []
-      return self.helperInorder(root, [])
+        if not root: return []
+        l,r=self.inorderTraversal(root.left),self.inorderTraversal(root.right)
+        return l+[root.val]+r
