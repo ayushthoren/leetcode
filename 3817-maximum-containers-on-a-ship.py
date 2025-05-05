44e5a14679a38d38d78a719d2b3d038233410de5
Ayush Thoren <ayushthoren@gmail.com>
2025-05-05 00:44:33 +0000

    Runtime: 3 ms, Memory 17.8 MB

--- 3817-maximum-containers-on-a-ship.py
+++ 3817-maximum-containers-on-a-ship.py
@@ -1,5 +1,3 @@
 class Solution:
     def maxContainers(self, n: int, w: int, maxWeight: int) -> int:
-        c,d=0,n**2
-        while maxWeight-w>=0 and c<d: maxWeight-=w; c+=1
-        return c
+        return n**2 if n**2*w<=maxWeight else maxWeight//w
