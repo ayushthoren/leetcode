011bfc84f43fdcc14cb47fc036d07514d226fd0a
Ayush Thoren <ayushthoren@gmail.com>
2025-05-10 16:05:44 +0000

    Runtime: 0 ms, Memory 18.4 MB

--- 414-third-maximum-number.py
+++ 414-third-maximum-number.py
@@ -1,5 +1,10 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-      nums=sorted(set(nums))
-      if len(nums)>2: return nums[::-1][2]
-      return nums[-1]
+        f,s,t=None,None,None
+        for i in nums:
+            if i==f or i==s or i==t: continue
+            if not f or i>f: f,s,t=i,f,s
+            elif not s or i>s: s,t=i,s
+            elif not t or i>t: t=i
+        print(f,s,t)
+        return t if t!=None else f
