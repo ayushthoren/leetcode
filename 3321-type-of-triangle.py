1b798a97892b26e453735ffeecdc115af79c3327
Ayush Thoren <ayushthoren@gmail.com>
2025-05-19 15:55:21 +0000

    Runtime: 0 ms, Memory 17.7 MB

---
+++ 3321-type-of-triangle.py
@@ -0,0 +1,6 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        if not (nums[0]+nums[1]>nums[2] and nums[0]+nums[2]>nums[1] and nums[1]+nums[2]>nums[0]): return "none"
+        if nums[0]==nums[1]==nums[2]: return "equilateral"
+        if nums[0]==nums[1] or nums[0]==nums[2] or nums[1]==nums[2]: return "isosceles"
+        return "scalene"
