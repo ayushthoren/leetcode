ba76256ee4faa9357fc75e6841b37fac582e2366
Ayush Thoren <ayushthoren@gmail.com>
2025-07-28 02:21:24 +0000

    Runtime: 31 ms, Memory 27.3 MB

--- 442-find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array.py
@@ -1,7 +1,8 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-      cts={}
-      for i in nums:
-        if i not in cts: cts[i]=0
-        cts[i]+=1
-      return [j for j in cts if cts[j]>1]
+        o = []
+        for n in nums:
+            i = abs(n) - 1
+            if nums[i] < 0: o.append(i + 1); continue
+            nums[i] *= -1
+        return o
