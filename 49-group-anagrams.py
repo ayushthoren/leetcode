4db5ed12fe3bfdf2e45e941f948728695dd2ac28
Ayush Thoren <ayushthoren@gmail.com>
2025-11-16 16:46:39 +0000

    Runtime: 11 ms, Memory 20.7 MB

--- 49-group-anagrams.py
+++ 49-group-anagrams.py
@@ -1,5 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = defaultdict(list)
-        for i in strs: d["".join(sorted(i))]+=[i]
+        for i in strs: d["".join(sorted(i))].append(i)
         return list(d.values())
