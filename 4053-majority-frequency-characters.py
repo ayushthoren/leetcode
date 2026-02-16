70459db8939297381c6d1681bbaffbadd0f74ba7
Ayush Thoren <ayushthoren@gmail.com>
2026-02-16 00:47:58 +0000

    Runtime: 2 ms, Memory 19.4 MB

--- 4053-majority-frequency-characters.py
+++ 4053-majority-frequency-characters.py
@@ -2,6 +2,5 @@ class Solution:
     def majorityFrequencyGroup(self, s: str) -> str:
         c, f = Counter(s), defaultdict(list)
         for l, n in c.items(): f[n].append(l)
-        print(f)
         m = max(f, key = lambda x: (len(f[x]), x))
         return "".join(f[m])
