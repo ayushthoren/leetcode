7bb2870372445fd338aad6b66136363f0302e293
Ayush Thoren <ayushthoren@gmail.com>
2026-05-04 02:41:40 +0000

    Runtime: 1 ms, Memory 19.2 MB

--- 4290-valid-elements-in-an-array.py
+++ 4290-valid-elements-in-an-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def findValidElements(self, nums: list[int]) -> list[int]:
-        p1, p2 = 0, len(nums)-1
         l, r = 0, 0
-        o = set()
-        while p2 >= 0:
-            if nums[p1] > l: l = nums[p1]; o.add(p1)
-            if nums[p2] > r: r = nums[p2]; o.add(p2)
-            p1 += 1; p2 -= 1
-        return [nums[i] for i in sorted(list(o))]
+        lr, rl = [], []
+        for i in range(len(nums)):
+            lr.append(l); rl.append(r)
+            if nums[i] > l: l = nums[i]
+            if nums[-(i+1)] > r: r = nums[-(i+1)]
+        rl.reverse()
+        return [nums[n] for n in range(len(nums)) if nums[n] > lr[n] or nums[n] > rl[n]]
