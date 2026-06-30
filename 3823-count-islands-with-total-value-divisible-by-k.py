8618aaa60dacd79d3be277fdb796fbf643c083f5
Ayush Thoren <ayushthoren@gmail.com>
2026-06-30 16:37:05 +0000

    Runtime: 290 ms, Memory 30.1 MB

--- 3823-count-islands-with-total-value-divisible-by-k.py
+++ 3823-count-islands-with-total-value-divisible-by-k.py
@@ -13,14 +13,12 @@ class Solution:
             s += zero(y, x)
 
             while q:
-                i = q[0]
-
+                i = q.popleft()
                 if i[0] - 1 >= 0 and grid[i[0]-1][i[1]] != 0: s += zero(i[0]-1, i[1])
                 if i[0] + 1 < len(grid) and grid[i[0]+1][i[1]] != 0: s += zero(i[0]+1, i[1])
                 if i[1] - 1 >= 0 and grid[i[0]][i[1]-1] != 0: s += zero(i[0], i[1]-1)
                 if i[1] + 1 < len(grid[i[0]]) and grid[i[0]][i[1]+1] != 0: s += zero(i[0], i[1]+1)
 
-                q.popleft()
             return s
 
         return sum(grid[y][x] != 0 and island(y, x) % k == 0 for y in range(len(grid)) for x in range(len(grid[y])))
